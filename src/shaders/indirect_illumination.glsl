d25980aac4117353b4cfbb9102c7052ea88dbc7f
Jaime Sierra <jaisiero@gmail.com>
2024-02-04 13:03:56 -0500

    Translating Scatter to II function

--- src/shaders/indirect_illumination.glsl
+++ src/shaders/indirect_illumination.glsl
@@ -9,18 +9,55 @@
 #extension GL_EXT_ray_query : enable
 
 void indirect_illumination(const daxa_i32vec2 index, const daxa_u32vec2 rt_size, INTERSECT i, daxa_u32 seed, daxa_u32 max_depth, daxa_u32 light_count, daxa_u32 object_count, inout daxa_f32vec3 throughput) {
+    
     // prd.throughput = vec3(1.0);
     prd.seed = seed;
     prd.depth = max_depth;
     prd.world_hit = i.world_hit;
     prd.distance = i.distance;
     prd.world_nrm = i.world_nrm;
-    prd.ray_scatter_dir = i.scatter_dir;
+    prd.ray_scatter_dir = i.wi;
+    prd.mat_index = i.material_idx;
     prd.instance_hit = i.instance_hit;
     // prd.done = true;
 
+#if RESERVOIR_ON == 1
+    call_scatter.hit = prd.world_hit;
+    call_scatter.nrm = prd.world_nrm;
+    call_scatter.ray_dir = i.wi;
+    call_scatter.seed = prd.seed;
+    call_scatter.scatter_dir = vec3(0.0);
+    call_scatter.done = false;
+    call_scatter.mat_idx = prd.mat_index;
+    call_scatter.instance_hit = i.instance_hit;
+
+
+    daxa_u32 mat_type = prd.mat_index & MATERIAL_TYPE_MASK;
+
+    switch (mat_type)
+    {
+    case MATERIAL_TYPE_METAL:
+        executeCallableEXT(3, 4);
+        break;
+    case MATERIAL_TYPE_DIELECTRIC:
+        executeCallableEXT(4, 4);
+        break;
+    case MATERIAL_TYPE_CONSTANT_MEDIUM:
+        executeCallableEXT(5, 4);
+        break;
+    case MATERIAL_TYPE_LAMBERTIAN:
+    default:
+        executeCallableEXT(2, 4);
+        break;
+    }
+    prd.seed = call_scatter.seed;
+    prd.world_hit = call_scatter.hit;
+    prd.world_nrm = call_scatter.nrm;
+    prd.ray_scatter_dir = call_scatter.scatter_dir;
+#endif // RESERVOIR_ON
+
     PATH_STATE path;
-    generate_path(path, index, rt_size, i.instance_hit, Ray(i.world_hit, i.scatter_dir), seed);
+    generate_path(path, index, rt_size, i.instance_hit, Ray(i.world_hit, i.wi), seed);
 
 
     // TODO: Do something with path here for primary hit
@@ -32,12 +69,12 @@ void indirect_illumination(const daxa_i32vec2 index, const daxa_u32vec2 rt_size,
             i.world_hit,
             i.world_nrm,
             i.distance,
-            i.scatter_dir,
+            i.wi,
             i.instance_hit,
             i.material_idx,
             seed,
             max_depth);
-        Ray ray = Ray(i.world_hit, i.scatter_dir);
+        Ray ray = Ray(i.world_hit, i.wi);
 
         if(i.is_hit) {
             
